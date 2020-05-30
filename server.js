51fdfeff98cbb8432fdec2fe0f43fc62bab1ad6a
W Biggs <wilson@wilsonbiggs.com>
2020-05-30 11:28:07 -0400

    Adding path to request logging

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/1212', {
 
     app.use((req, res, next) => {
       const origins = ['http://localhost:3000', 'http://localhost:3000/', 'https://1212.one', 'https://1212.one/'];
-      console.log(`Origin: ${req.headers.origin}, IP: ${req.headers['x-forwarded-for']}`);
+      console.log(`Origin: ${req.headers.origin}, IP: ${req.headers['x-forwarded-for']}, Path: ${req.path}`);
       if (origins.indexOf(req.headers.origin) >= 0) {
         res.header('Access-Control-Allow-Origin', req.headers.origin);
       }
